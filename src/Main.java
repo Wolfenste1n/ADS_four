1d0f573da50e243a7eeac7329e1a4fb3ddf82b32
Madiyar <wolfenstrun@gmail.com>
2025-05-18 12:30:05 +0500

    Commit Main final

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println(123);
+        MyGraph<String> graph = new MyGraph<>();
+
+        graph.addEdge("A", "B", 4);
+        graph.addEdge("A", "C", 2);
+        graph.addEdge("B", "C", 1);
+        graph.addEdge("B", "D", 5);
+        graph.addEdge("C", "D", 8);
+        graph.addEdge("C", "E", 10);
+        graph.addEdge("D", "E", 2);
+        graph.addEdge("D", "Z", 6);
+        graph.addEdge("E", "Z", 3);
+
+        Vertex<String> source = graph.getVertex("A");
+        Vertex<String> destination = graph.getVertex("Z");
+
+        System.out.println("BFS:");
+        BreadthFirstSearch<String> bfs = new BreadthFirstSearch<>(graph, source);
+        bfs.search();
+        System.out.println("Path from A to Z:");
+        for (Vertex<String> v : bfs.getPathTo(destination)) {
+            System.out.print(v + " ");
+        }
+
+        System.out.println("\n\nDijkstra:");
+        DijkstraSearch<String> dijkstra = new DijkstraSearch<>(graph, source);
+        dijkstra.search();
+        System.out.println("Shortest path from A to Z:");
+        for (Vertex<String> v : dijkstra.getPathTo(destination)) {
+            System.out.print(v + " ");
+        }
     }
 }
